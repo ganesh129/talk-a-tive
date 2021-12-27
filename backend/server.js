fb6e5c089dba38d3037ef1ecfd48fca47b71bd94
Jing Qiang <tohjingqiang@gmail.com>
2021-12-27 18:31:15 +0800

    login and signup api

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,20 @@ const express = require('express');
 const dotenv = require('dotenv')
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
+
 dotenv.config();
 connectDB();
+
+app.use(express.json()); // to accept JSON data
 
 app.get('/', (req, res) => {
     res.send("API is Running Successfully");
 })
 
-app.get('/api/chat', (req, res) => {
-    res.send(chats);
-});
-
-app.get('/api/chat/:id', (req, res) => {
-    const singleChat = chats.find((c) => c._id === req.params.id);
-    res.send(singleChat);
-})
+app.use('/api/user', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
