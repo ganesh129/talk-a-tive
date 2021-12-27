8be8eb0b3e7c7a5c8fc8dec6c82bebc2b6abfb45
Jing Qiang <tohjingqiang@gmail.com>
2021-12-27 18:36:03 +0800

    error handling

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv')
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
 const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express();
 
@@ -15,7 +16,10 @@ app.get('/', (req, res) => {
     res.send("API is Running Successfully");
 })
 
-app.use('/api/user', userRoutes)
+app.use('/api/user', userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000
 
