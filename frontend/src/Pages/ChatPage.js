a41887f994401388d5b4ca83933983f7f082fda4
Jing Qiang <tohjingqiang@gmail.com>
2021-12-27 15:29:02 +0800

    fetching backend data to frontend

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ChatPage = () => {
+    const [chats, setChats] = useState([])
+
+    const fetchChats = async () => {
+        const { data } = await axios.get('/api/chat');
+
+        setChats(data);
+    };
+
+    useEffect(() => {
+        fetchChats();
+    }, [])
+
     return (
         <div>
-            ChatPage
+            {chats.map((chat) => {
+                return <div key={chat._id}>{chat.chatName}</div>
+            })}
         </div>
-    )
-}
+    );
+};
 
 export default ChatPage
