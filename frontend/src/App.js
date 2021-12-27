4133e4b1b184ade6423a585e8d1a33b289f074db
Jing Qiang <tohjingqiang@gmail.com>
2021-12-27 15:13:39 +0800

    pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+import { Route } from 'react-router-dom';
 import './App.css';
+import ChatPage from './Pages/ChatPage';
+import Homepage from './Pages/Homepage';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Route path='/' component={Homepage} exact />
+      <Route path='/chats' component={ChatPage}/>
     </div>
   );
 }
