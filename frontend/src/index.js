62e7b0492d143afb21a36bff25ed024c8dec9690
Jing Qiang <tohjingqiang@gmail.com>
2021-12-28 15:28:08 +0800

    creating context

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { ChakraProvider } from "@chakra-ui/react"
 import { BrowserRouter } from "react-router-dom"
+import ChatProvider from './Context/ChatProvider';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ChatProvider>
     <BrowserRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </BrowserRouter>
-  </React.StrictMode>,
+  </ChatProvider>,
   document.getElementById('root')
 );
