a5d642dfec22afd560389b5dcfd12ac5e5e9f48a
Jing Qiang <tohjingqiang@gmail.com>
2021-12-28 14:01:41 +0800

    Login component

--- frontend/src/components/Authentication/Login.js
+++ frontend/src/components/Authentication/Login.js
@@ -10,66 +10,70 @@ import { useHistory } from "react-router-dom";
 const Login = () => {
     // States //
     const [show, setShow] = useState(false);
-    const toast = useToast();
-    const [email, setEmail] = useState();
-    const [password, setPassword] = useState();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
     const [loading, setLoading] = useState(false);
 
-
-    // Functions //
-    const handleClick = () => setShow(!show);
-
+    const toast = useToast();
     const history = useHistory();
 
+    // Functions //
+    // hide or show password
+    const handleClick = () => setShow(!show);
+
+    // Log in
     const submitHandler = async () => {
         setLoading(true);
+
+        // if not all fields inputted
         if (!email || !password) {
-        toast({
-            title: "Please Fill all the Feilds",
-            status: "warning",
-            duration: 5000,
-            isClosable: true,
-            position: "bottom",
-        });
-        setLoading(false);
-        return;
+            toast({
+                title: "Please Fill all the Fields",
+                status: "warning",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
+            return;
         }
 
-    // console.log(email, password);
-    try {
-        const config = {
-            headers: {
-            "Content-type": "application/json",
-            },
-        };
+        // POST request
+        try {
+            const config = {
+                headers: {
+                "Content-type": "application/json",
+                },
+            };
 
-        const { data } = await axios.post(
-            "/api/user/login",
-            { email, password },
-            config
-        );
+            const { data } = await axios.post(
+                "/api/user/login",
+                { email, password },
+                config
+            );
 
-        // console.log(JSON.stringify(data));
-        toast({
-            title: "Login Successful",
-            status: "success",
-            duration: 5000,
-            isClosable: true,
-            position: "bottom",
-        });
-        localStorage.setItem("userInfo", JSON.stringify(data));
-        setLoading(false);
-        history.push("/chats");
+            // login successful
+            toast({
+                title: "Login Successful",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            localStorage.setItem("userInfo", JSON.stringify(data));
+
+            setLoading(false);
+            history.push("/chats");
         } catch (error) {
-        toast({
-            title: "Error Occured!",
-            description: error.response.data.message,
-            status: "error",
-            duration: 5000,
-            isClosable: true,
-            position: "bottom",
-        });
-        setLoading(false);
+            toast({
+                title: "Error Occured!",
+                description: error.response.data.message,
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "bottom",
+            });
+            setLoading(false);
         }
     };
 
