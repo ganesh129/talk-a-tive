0c6e244e4737f0b07c8db39d166633b0d43d0f0d
Jing Qiang <tohjingqiang@gmail.com>
2021-12-28 22:31:02 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
+const path = require("path");
 
 const app = express();
 
@@ -13,21 +14,39 @@ connectDB();
 
 app.use(express.json()); // to accept JSON data
 
-// app.get('/', (req, res) => {
-//     res.send("API is Running Successfully");
-// })
 
 app.use('/api/user', userRoutes);
 app.use('/api/chat', chatRoutes);
 app.use('/api/message', messageRoutes);
 
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
+
 // Error Handling middlewares
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT;
 
-const server = app.listen(PORT, console.log(`Server Started on PORT ${PORT}`));
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
 
 const io = require('socket.io')(server, {
     pingTimeout:60000,
