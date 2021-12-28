278e454873704ea7276c4023ac84233d0139ca86
Jing Qiang <tohjingqiang@gmail.com>
2021-12-28 15:37:45 +0800

    useContext Hook

--- frontend/src/Pages/Homepage.js
+++ frontend/src/Pages/Homepage.js
@@ -2,8 +2,18 @@ import React from 'react'
 import { Box, Container, Tab, TabList, TabPanel, TabPanels, Tabs, Text } from "@chakra-ui/react"
 import Login from '../components/Authentication/Login'
 import Signup from '../components/Authentication/Signup'
+import { useHistory } from 'react-router-dom';
+import { useEffect } from 'react'
 
 const Homepage = () => {
+    const history = useHistory();
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem("userInfo"));
+
+        if (user) history.push("/chats");
+    }, [history]);
+
     return (
         <Container maxW='xl' centerContent>
             <Box
